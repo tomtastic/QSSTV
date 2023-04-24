788009eb0911f6ab3e7d52eb85e0cbf8fa7afc7c
Mario Klebsch <mario@klebsch.de>
2023-04-24 18:36:08 +0200

    Added support for macos CoreAudio, pulse isn't used on macos anymore.

---
+++ src/macos/CaptureDeviceAuthorization.h
@@ -0,0 +1,12 @@
+//
+//  CaptureDeviceAuthorization.h
+//  qsstv
+//
+//  Created by Mario Klebsch on 23.04.23.
+//
+
+#ifndef CaptureDeviceAuthorization_h
+#define CaptureDeviceAuthorization_h
+
+
+#endif /* CaptureDeviceAuthorization_h */
