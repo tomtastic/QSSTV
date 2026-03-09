5234782a9d73c11fc6c3d55ecd0381e331312c28
Tom Matthews <trcm@pm.me>
2026-03-09 16:03:00 +0000

    Remove unused includes from downsamplefilter.cpp

--- src/dsp/downsamplefilter.cpp
+++ src/dsp/downsamplefilter.cpp
@@ -19,8 +19,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include "downsamplefilter.h"
-#include "appglobal.h"
-#include "arraydumper.h"
 #include "logging.h"
 
 #include <QDebug>
